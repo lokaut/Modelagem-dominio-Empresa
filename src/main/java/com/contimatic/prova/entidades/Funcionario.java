eedd3f0c7be28e067462ecfc9f19c35461185e9c
Antonio Erick Vieira Rodrigues <erick_brasil22@hotmail.com>
2021-08-10 16:55:04 -0300

    classe funcionarioValidacao

--- src/main/java/com/contimatic/prova/entidades/Funcionario.java
+++ src/main/java/com/contimatic/prova/entidades/Funcionario.java
@@ -1,6 +1,8 @@
 package com.contimatic.prova.entidades;
 
-import com.contimatic.prova.testes.funcionarios.FuncionarioTeste;
+import java.util.Objects;
+
+import com.contimatic.prova.testes.funcionarios.FuncionarioValidacao;
 
 public class Funcionario {
 
@@ -23,28 +25,35 @@ public class Funcionario {
 	}
 
 	public void setNome(String nome) {
-		FuncionarioTeste.sessantaCaracter(nome);
-		FuncionarioTeste.nomeNulo(nome);
-		FuncionarioTeste.nomeEmBranco(nome);
+		FuncionarioValidacao.sessantaCaracter(nome);
+		FuncionarioValidacao.nomeNulo(nome);
+		FuncionarioValidacao.nomeEmBranco(nome);
 	}
 
 
 
-	public double getSalario() {
-		return salario;
+	@Override
+	public int hashCode() {
+		return Objects.hash(Endereco, cpf, nome, salario);
 	}
 
-	public void setSalario(double salario) {
-		this.salario = salario;
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)return true;
+		if (obj == null)return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		return Objects.equals(Endereco, other.Endereco) && Objects.equals(cpf, other.cpf)
+				&& Objects.equals(nome, other.nome)
+				&& Double.doubleToLongBits(salario) == Double.doubleToLongBits(other.salario);
 	}
 
-	public Endereco getEndereco() {
-		return Endereco;
-	}
 
-	public void setEndereco(Endereco endereco) {
-		Endereco = endereco;
-	}
+
+	
 
 	
 }
--- src/main/java/com/contimatic/prova/testes/funcionarios/FuncionarioValidacao.java
+++ src/main/java/com/contimatic/prova/testes/funcionarios/FuncionarioValidacao.java
@@ -2,27 +2,27 @@ package com.contimatic.prova.testes.funcionarios;
 
 import com.contimatic.prova.entidades.Funcionario;
 
-/**
- * Hello world!
- *
- */
-public class FuncionarioTeste {
+
+public class FuncionarioValidacao {
 
 	Funcionario funcionario = new Funcionario();
 
 	public static void sessantaCaracter(String nome) {
 		if (nome != null) {
-			if ((nome.length() <= 2 || nome.length() >= 60)) {
-				//illegalstatt
+			if ((nome.length() < 3 || nome.length() > 60)) {
+				//illegalstate
 				throw new IllegalArgumentException("Quantidade de carácter inválido, o Nome deve estar entre 2 a 60 caracteres");
 			}
 		}
 	}
 	
 	//nonNull
-	public static void nomeNulo(String nome) {
+	public static boolean nomeNulo(String nome) {
 		if(nome == null) {
 			throw new IllegalArgumentException("campo não pode ser nulo" );
+		}
+		else {
+			return true;
 		}
 
 	}
