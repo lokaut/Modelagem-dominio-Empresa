6c4ac6b4e20284f7da4a78eba0f12009ae7ed33f
Erick Rodrigues <erick_brasil22@hotmail.com>
2021-10-19 16:12:38 -0300

    Adição de constantes cpf

--- src/main/java/com/contimatic/prova/constantes/Constantes.java
+++ src/main/java/com/contimatic/prova/constantes/Constantes.java
@@ -3,5 +3,7 @@ package com.contimatic.prova.constantes;
 public class Constantes {
 	// 62 caracteres
 	public static final String SESSENTA_CARACTER = "contmatic contmatic contmatic contmatic contmatic contmaticsss";
-
+	public static final int POSICAO_ZERO_ASCII = 48;
+	public static final int PESO_ONZE_CPF = 11;
+	public static final int PESO_DEZ_CPF = 10;
 }
