86e8f5f1379f1e3a2f7cdb5da95bed5c2ed01a43
Erick Rodrigues <erick_brasil22@hotmail.com>
2021-10-25 12:19:50 -0300

    atualização das contantes

--- src/main/java/com/contimatic/prova/constantes/Constantes.java
+++ src/main/java/com/contimatic/prova/constantes/Constantes.java
@@ -4,6 +4,10 @@ public  final class Constantes {
 	private  Constantes(){}
 	
 	public static final String SESSENTA_CARACTER = "contmatic contmatic contmatic contmatic contmatic contmaticsss";
+	public static final String DOIS_CARACTER = "ac";
+	public static final String NUMERO_CPF_INVALIDO = "44085620000";
+	
+	/* CPF */
 	public static final int POSICAO_ZERO_ASCII = 48;
 	public static final int PESO_ONZE_CPF = 11;
 	public static final int PESO_DEZ_CPF = 10;
@@ -11,4 +15,14 @@ public  final class Constantes {
 	public static final int POSICAO_DEZ_CPF = 10;
 	public static final int TAMANHO_CPF = 11;
 	public static final char NUMERO_ZERO_CPF = '0';
+	public static final String CPF_VALIDO = "90795007809"; //76899070081
+	
+	/*Mensagens */
+	public static final String POSSUI_CARACTER_ESPECIAL_NUMERICO = "O Campo possui caracter númerico ou caracter especial";
+	public static final String CAMPO_NULO = "Este campo não pode ser nulo";
+	public static final String CAMPO_VAZIO = "Campo está vazio ou contém apenas espaço em branco";
+	public static final String CPF_INVALIDO = "CPF invalido";
+	public static final String CPF_DIFERENTE_ONZE_NUMEROS = "CPF inválido, pois não possui 11 caracteres";
+	public static final String EMAIL_INVALIDO = "Email Inválido";
+	public static final String CPF_INVALIDO_LETRAS = "CPF deve possuir apenas números";
 }
