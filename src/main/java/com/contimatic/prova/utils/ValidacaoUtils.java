a4993249e5ba918a74007dae3e9ff43c13bf4579
Erick Rodrigues <erick_brasil22@hotmail.com>
2021-10-25 12:20:23 -0300

    refatoração de constantes

--- src/main/java/com/contimatic/prova/utils/ValidacaoUtils.java
+++ src/main/java/com/contimatic/prova/utils/ValidacaoUtils.java
@@ -1,13 +1,20 @@
 package com.contimatic.prova.utils;
 
+import static com.contimatic.prova.constantes.Constantes.POSSUI_CARACTER_ESPECIAL_NUMERICO;
+import static com.contimatic.prova.constantes.Constantes.CAMPO_NULO;
+import static com.contimatic.prova.constantes.Constantes.CAMPO_VAZIO;
+import static com.contimatic.prova.constantes.Constantes.EMAIL_INVALIDO;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class ValidacaoUtils {
 
-	private ValidacaoUtils() {
-	}
+	private ValidacaoUtils() {}
 
-	public static void verificacaoCampoNulo(Object nome) {
+	public static void verificarCampoNulo(Object nome) {
 		if (nome == null) {
-			throw new IllegalArgumentException("Este campo não pode ser nulo");
+			throw new IllegalArgumentException(CAMPO_NULO);
 		}
 	}
 
@@ -20,15 +27,23 @@ public final class ValidacaoUtils {
 	}
 
 	public static void naoAceitarCampoEmBranco(String nome) {
-		if (nome.isBlank()) {
-			throw new IllegalStateException("Campo está vazio ou contém apenas espaço em branco");
+		if (nome.trim().isBlank()) {
+			throw new IllegalStateException(CAMPO_VAZIO);
 		}
 	}
 
 	public static void naoAceitarCaracterNumerico(String nome) {
 		if (!nome.matches("[a-zA-Z_ ]{1,60}")) {
-			throw new IllegalStateException("O Campo possui caracter númerico ou caracter especial");
+			throw new IllegalStateException(POSSUI_CARACTER_ESPECIAL_NUMERICO);
 		}
 	}
-
+	
+	public static void validarEmail(String email) {
+			String expression = "^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+			Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(email);
+			if (!matcher.matches()) {
+				throw new IllegalStateException(EMAIL_INVALIDO);
+			}
+	}
 }
