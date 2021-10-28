aef112a1259a6df1f4652a5a856af334e7f00a93
Erick Rodrigues <erick_brasil22@hotmail.com>
2021-10-27 22:28:41 -0300

    Update TestesUnitariosFuncionario.java

--- src/test/java/com/contimatic/prova/TestesUnitariosFuncionario.java
+++ src/test/java/com/contimatic/prova/TestesUnitariosFuncionario.java
@@ -4,14 +4,18 @@ import static com.contimatic.prova.constantes.Constantes.CAMPO_NULO;
 import static com.contimatic.prova.constantes.Constantes.CAMPO_VAZIO;
 import static com.contimatic.prova.constantes.Constantes.CPF_DIFERENTE_ONZE_NUMEROS;
 import static com.contimatic.prova.constantes.Constantes.CPF_INVALIDO;
+import static com.contimatic.prova.constantes.Constantes.CPF_TEXTO;
 import static com.contimatic.prova.constantes.Constantes.CPF_VALIDO;
 import static com.contimatic.prova.constantes.Constantes.DOIS_CARACTER;
 import static com.contimatic.prova.constantes.Constantes.EMAIL_INVALIDO;
 import static com.contimatic.prova.constantes.Constantes.POSSUI_CARACTER_ESPECIAL_NUMERICO;
+import static com.contimatic.prova.constantes.Constantes.SALARIO_MENOR_SALARIO_MINIMO;
 import static com.contimatic.prova.constantes.Constantes.SESSENTA_CARACTER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,16 +27,20 @@ class TestesUnitariosFuncionario {
 
 	private Funcionario funcionario;
 	private String nome;
+
 	private IllegalStateException illegalState;
 	private IllegalArgumentException illegalArgument;
 
+	private BigDecimal salario1 = new BigDecimal(1110.0);
+	private BigDecimal salario2 = new BigDecimal(800.2);
+
 	@BeforeEach
 	public void instancia() {
+
 		funcionario = new Funcionario();
-		// aplicação dos testes
 		nome = "Lokaut";
 	}
-	
+
 	@AfterAll
 	public static void finalização() {
 		System.out.println("Fim dos testes funcionario");
@@ -40,13 +48,13 @@ class TestesUnitariosFuncionario {
 
 	@Test
 	void deve_validar_mais_sessenta_caracteres_nome() {
-		assertThrows(IllegalStateException.class,() -> funcionario.setNome(SESSENTA_CARACTER));
+		assertThrows(IllegalStateException.class, () -> funcionario.setNome(SESSENTA_CARACTER));
 	}
-	
+
 	@Test
 	void deve_validar_menos_tres_caracteres_nome() {
-		 assertThrows(IllegalStateException.class,() -> funcionario.setNome(DOIS_CARACTER));
-	}	
+		assertThrows(IllegalStateException.class, () -> funcionario.setNome(DOIS_CARACTER));
+	}
 
 	@Test
 	void deve_validar_campo_nome_valido() {
@@ -56,28 +64,22 @@ class TestesUnitariosFuncionario {
 
 	@Test
 	void deve_validar_campo_nome_nulo() {
-		illegalArgument = assertThrows(IllegalArgumentException.class, () -> funcionario.setNome(null));
-        assertTrue(illegalArgument.getMessage().contains(CAMPO_NULO));
+		this.illegalArgument = assertThrows(IllegalArgumentException.class, () -> funcionario.setNome(null));
+		assertTrue(this.illegalArgument.getMessage().contains(CAMPO_NULO));
 	}
 
 	@Test
 	void deve_validar_campo_em_branco_nome() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setNome(" "));
-		assertTrue(illegalState.getMessage().contains(CAMPO_VAZIO));
+		this.illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setNome(" "));
+		assertTrue(this.illegalState.getMessage().contains(CAMPO_VAZIO));
 	}
 
 	@Test
 	void deve_validar_caracter_numerico_nome() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setNome("123456"));
-		assertTrue(illegalState.getMessage().contains(POSSUI_CARACTER_ESPECIAL_NUMERICO));
+		this.illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setNome("123456"));
+		assertTrue(this.illegalState.getMessage().contains(POSSUI_CARACTER_ESPECIAL_NUMERICO));
 	}
-	
-	@Test
-	void deve_validar_cpf_invalido() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("40000000000"));
-		assertTrue(illegalState.getMessage().contains(CPF_INVALIDO));
-	}
-	
+
 	@Test
 	void deve_validar_cpf_correto() {
 		funcionario.setCpf(CPF_VALIDO);
@@ -86,31 +88,61 @@ class TestesUnitariosFuncionario {
 
 	@Test
 	void deve_validar_cpf_menos_onze_numeros() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("440856"));
-		assertTrue(illegalState.getMessage().contains(CPF_DIFERENTE_ONZE_NUMEROS));
+		this.illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("440856"));
+		assertTrue(this.illegalState.getMessage().contains(CPF_DIFERENTE_ONZE_NUMEROS));
 	}
-	
+
 	@Test
 	void deve_validar_cpf_numero_igual() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("00000000000"));
-		assertTrue(illegalState.getMessage().contains(CPF_INVALIDO));
+		this.illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("00000000000"));
+		assertTrue(this.illegalState.getMessage().contains(CPF_INVALIDO));
 	}
-	
+
 	@Test
 	void deve_validar_caracter_texto_cpf() {
-		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf("44a764b7f34"));
+		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setCpf(CPF_TEXTO));
 		assertTrue(illegalState.getMessage().contains(CPF_INVALIDO));
 	}
-	
+
+	@Test
+	void deve_validar_campo_nulo_cpf() {
+		this.illegalArgument = assertThrows(IllegalArgumentException.class, () -> funcionario.setCpf(null));
+		assertTrue(this.illegalArgument.getMessage().contains(CAMPO_NULO));
+	}
+
 	@Test
 	void deve_validar_email_incorreto() {
 		illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setEmail("erick22@.com"));
 		assertTrue(illegalState.getMessage().contains(EMAIL_INVALIDO));
 	}
-	
+
 	@Test
 	void deve_validar_email_valido() {
 		funcionario.setEmail("erick224@gmail.com");
 		assertEquals("erick224@gmail.com", funcionario.getEmail());
+	}
+
+	@Test
+	void deve_validar_campo_nulo_email() {
+		this.illegalArgument = assertThrows(IllegalArgumentException.class, () -> funcionario.setEmail(null));
+		assertTrue(this.illegalArgument.getMessage().contains(CAMPO_NULO));
+	}
+
+	@Test
+	void deve_validar_campo_nulo_salario() {
+		this.illegalArgument = assertThrows(IllegalArgumentException.class, () -> funcionario.setSalario(null));
+		assertTrue(this.illegalArgument.getMessage().contains(CAMPO_NULO));
+	}
+
+	@Test
+	void deve_validar_salario_menor_salario_minimo() {
+		this.illegalState = assertThrows(IllegalStateException.class, () -> funcionario.setSalario(salario2));
+		assertTrue(this.illegalState.getMessage().contains(SALARIO_MENOR_SALARIO_MINIMO));
+	}
+
+	@Test
+	void deve_validar_salario_valido() {
+		funcionario.setSalario(salario1);
+		assertEquals(salario1, funcionario.getSalario());
 	}
 }
