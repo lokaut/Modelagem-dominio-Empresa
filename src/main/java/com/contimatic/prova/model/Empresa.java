001664b3260f95aea2890c38a6e98dd8170b98b6
Erick Rodrigues <erick_brasil22@hotmail.com>
2021-11-04 09:12:13 -0300

    criação da classe Empresa

---
+++ src/main/java/com/contimatic/prova/model/Empresa.java
@@ -0,0 +1,66 @@
+package com.contimatic.prova.model;
+
+import java.time.LocalDate;
+
+public class Empresa {
+	private String cnpj;
+
+	private String razaoSocial;
+
+	private String nomeFantasia;
+
+	private String porte;
+
+	private LocalDate dataAbertura;
+	
+	private Funcionario funcionario;
+
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getPorte() {
+		return porte;
+	}
+
+	public void setPorte(String porte) {
+		this.porte = porte;
+	}
+
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDate dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+}
