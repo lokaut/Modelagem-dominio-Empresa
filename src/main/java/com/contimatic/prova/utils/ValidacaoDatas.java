a9bd005c815f34a9bc17dd5ff64486f4edd9a65e
Erick Rodrigues <erick_brasil22@hotmail.com>
2022-01-07 14:32:12 -0300

    adição do import estatico

--- src/main/java/com/contimatic/prova/utils/ValidacaoDatas.java
+++ src/main/java/com/contimatic/prova/utils/ValidacaoDatas.java
@@ -4,23 +4,24 @@ import static com.contimatic.prova.constantes.Constantes.IDADE_MINIMA_EMPRESA;
 import static com.contimatic.prova.constantes.Constantes.MENSAGEM_ADMISSAO_FUTURA;
 import static com.contimatic.prova.constantes.Constantes.MENSAGEM_IDADE_MINIMA_EMPRESA;
 import static com.contimatic.prova.constantes.Constantes.REGRA_DATA_ADMISSAO;
+import static java.time.LocalDate.now;
+import static java.time.temporal.ChronoUnit.YEARS;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 public final class ValidacaoDatas {
 	private ValidacaoDatas() {}
 
 	public static void dataNascMaiorIdade(LocalDate dataNascimento) {
-		LocalDate dataAtual = LocalDate.now();
-		long anos = ChronoUnit.YEARS.between(dataNascimento, dataAtual);
+		LocalDate dataAtual = now();
+		long anos = YEARS.between(dataNascimento, dataAtual);
 		if (anos < IDADE_MINIMA_EMPRESA) {
 			throw new IllegalStateException(MENSAGEM_IDADE_MINIMA_EMPRESA);
 		}
 	}
 
 	public static void validacaoDataAdmissao(LocalDate dataAdmissao) {
-		LocalDate doisMesesPosDataAtual = LocalDate.now().plusMonths(REGRA_DATA_ADMISSAO);
+		LocalDate doisMesesPosDataAtual = now().plusMonths(REGRA_DATA_ADMISSAO);
 		if (dataAdmissao.isAfter(doisMesesPosDataAtual)) {
 			throw new IllegalStateException(MENSAGEM_ADMISSAO_FUTURA);
 		}
